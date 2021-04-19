f813bc8c9fdd4582d722e888afcc9a3ea49de122
KanshiinQc <77125624+KanshiinQc@users.noreply.github.com>
2021-04-19 14:48:51 -0400

    ajout de features de reponse du bot

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
-# bot.py
+import random
 import os
-
 import discord
 from dotenv import load_dotenv
+
+# 1
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# 2
+bot = commands.Bot(command_prefix='!')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -17,11 +27,52 @@ async def on_ready():
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
+        f'{client.user} is connected to the following guild: \n' 
+        f'{guild.name} (id: {guild.id})'
     )
+
+    # just trying to debug here
+    for guild in client.guilds:
+        for member in guild.members:
+            print(member.name, ' ')
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+    
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+    if 'happy birthday' or 'hb' in message.content.lower():
+        await message.channel.send('t po bo mais cé po rave')
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise 
 
 client.run(TOKEN)
