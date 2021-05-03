ffaf77aee429ea2478ac6a3a22f49831d40fd420
KanshiinQc <77125624+KanshiinQc@users.noreply.github.com>
2021-05-03 12:43:33 -0400

    maj commandes

--- bot.py
+++ bot.py
@@ -15,12 +15,6 @@ GUILD = os.getenv('DISCORD_GUILD')
 bot = commands.Bot(command_prefix='!')
 
 # Commande du bot
-# Affiche des meme
-@bot.command(name='meme')
-async def meme(ctx):
-    reponsejson = requests.get(f'http://alpha-meme-maker.herokuapp.com/memes/{random.randrange(0 , 150)}').json()
-    await ctx.send(reponsejson['data']['image'])
-
 @bot.command(name='actu')
 async def actu(ctx):
     reponseActu = requests.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnews.ycombinator.com%2Frss').json()
@@ -44,18 +38,14 @@ async def chien(ctx, arg):
 async def chuck(ctx):
     reponsejson = requests.get('https://api.chucknorris.io/jokes/random').json()
     await ctx.send(reponsejson['value'])
-# Pour Étienne 
-@bot.command(name='patate')
-async def patate(ctx, arg):
-    reponsePatate = "DÉ PETATES!"
-    await ctx.send(reponsePatate + ' ' + arg)
 
-# Fait la multipication de deux nombres
-@bot.command(name='multiply')
-async def multiply(ctx, arg, arg2):
-    paulKing = 'resultat ='
-    await ctx.send(f'{paulKing} {(int(arg) * int(arg2))}')
-
+# Affiche des meme
+@bot.command(name='meme')
+async def meme(ctx):
+    reponsejson = requests.get(f'http://alpha-meme-maker.herokuapp.com/memes/{random.randrange(0 , 150)}').json()
+    await ctx.send(reponsejson['data']['image'])
+    
+# Roche Papier Ciseau
 @bot.command(name='rpc')
 async def rpc(ctx, arg):
     result = ""
@@ -90,6 +80,4 @@ async def rpc(ctx, arg):
     
     message = answerReturned + ", " + result
     await ctx.send(f'{message}')
-
-
 bot.run(TOKEN)
